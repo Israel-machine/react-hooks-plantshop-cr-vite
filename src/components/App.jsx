8c1ef4347ea372ee10df25136ac5581112797bc1
Israel-machine <israel.diaz1701@gmail.com>
2026-03-11 14:54:43 -0700

    fetch request attached to useEffect to return data from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[]);
+
+  
   return (
     <div className="app">
       <Header />
