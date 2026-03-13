1ff7632b0c6dc729a6fcc799be52f788e29b0795
Israel-machine <israel.diaz1701@gmail.com>
2026-03-12 22:06:34 -0700

    Plant Shop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,7 @@ import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:6001/plants")
-    .then(res => res.json())
-    .then(data => console.log(data))
-  },[]);
 
-  
   return (
     <div className="app">
       <Header />
